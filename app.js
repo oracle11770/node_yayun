e110a4f7b423a88e8f785cbe4012d4e71a7c180d
89b_100006563470_oracle11770 <89b_100006563470_oracle11770@git.cloud.tencent.com>
2019-03-10 16:16:42 +0800

    'yxyinit'

---
+++ app.js
@@ -0,0 +1,28 @@
+// 引入模块
+var express = require('express');
+var path = require('path');
+var ejs = require('ejs');
+ 
+var app = express();
+ 
+// 预加载路由处理
+// var router = require('./router/router')
+// router.init(app)
+app.use('/',function (req, res) {
+    res.render('index.html');
+})
+
+// 设置views路径和模板
+app.set('views', './views');
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+ 
+// 静态文件配置
+app.use('/static', express.static(path.join(__dirname, '/static')));
+ 
+// 启动一个服务，监听从8888端口进入的所有连接请求
+var server = app.listen(3000, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Listening at', host, port);
+}); 
