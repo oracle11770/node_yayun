56df7120348c0fa326c0e443a507e8fa48e879b8
yxy <oracle11770@hotmail.com>
2019-11-22 01:36:22 +0800

    yxy modify

--- router/index.js
+++ router/index.js
@@ -7,50 +7,55 @@ router.get("", async function (req, res) {
     });
 });
 router.get("/query", async function (req, res) {
-    // console.log('req.body',req.params, req.body, req.query)
+    console.log('req.body',req.params, req.body, req.query)
     // req.query.word = urlencode(req.query.word)
     const isRender = req.query.isRender === 'false' ? true : false
+    const source = req.query.source
     delete req.query.isRender
     let resultData = {}
-    if(req.query.source && 
-        (req.query.source === 'tang' || req.query.source === "song" || req.query.source === "yuan")){
+    let curi = 1
+    let total = 0
+    var html = []
+
+    if (source && 
+        (source === 'tang' || source === "song" || source === "yuan")){
         resultData = await indexObj.getQueryVerse(req.query)
+        resultData = resultData.data
+        total = resultData.total
+        let fromi = req.query.fromi
+        if (fromi < total) {
+            curi = fromi > 6 ? (fromi - 6) : curi
+        } else {
+            curi = total - total % 10
+        }
+       resultData.body.forEach(function (item) {
+            item.body = item.body.split('。')
+        });
+        html = resultData.body
     }else{
+        delete req.query.source
         resultData = await indexObj.getQueryResult(req.query)
+        html = resultData.data;
     }
     console.log(resultData,'==========',req.query); 
-    let curi = 0
-    let total = resultData.data.total
-    let fromi = req.query.fromi
-    if (fromi < total) {
-        curi = fromi > 6 ? (fromi - 6) : curi
-    } else {
-        curi = total - total % 10
-    }
-    var html = []
-    resultData.data.body.forEach(function (item) {
-        var body = item.body.split('。').join('。<br/>')
-        html.push(`<div class='verDiv'>
-                <p>${item.title}</p>
-                <p>作者：&nbsp;${item.author}</p>
-                <p>${body}</p>
-        </div>`)
-    });
+  
+    
+   
     if(isRender){
         // console.log(99999999999,resultData)
-        console.log('=======99999999999', isRender, JSON.stringify(resultData))
+        console.log('=======99999999999', isRender, html)
         await res.send({
             queryName: req.query.word,
             data: html,
-            source: req.query.source || "",
+            source: source || "",
             total,
             curi
         })
     }else{
-        console.log('=======', isRender, JSON.stringify(resultData))
+        console.log('=======', isRender, html)
         await res.render('response.ejs',{
             data: html,
-            source: req.query.source || "",
+            source: source || "",
             total,
             curi,
             queryName: req.query.word,
