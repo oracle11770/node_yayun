b65a2d2d9737b6cd4f8de4ffbed4d7058b66888e
yangxy <yangxy@ikongjian.com>
2019-12-21 17:57:59 +0800

    yxy 删除字

--- models/index.js
+++ models/index.js
@@ -4,10 +4,6 @@ module.exports = {
     getQueryResult: async (params) => {
         let data = ''
         await axios.get(api+'/index/api/yayun',{params}).then((res) => {
-            params.size = 10;
-            if(Number(params.curi)){
-                params.from = params.curi
-            }
             // console.log('res.status',res.status, typeof res.status)
             if (res.status === 200) {
                 data = res.data
